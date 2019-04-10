67b95a88393af98eb6a3efd220c510e9700fb78b
anjasmoro318 <anjasmoro318@gmail.com>
2019-04-10 12:00:09 +0700

    modul_4

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,31 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+Route::get('/welcome', function () {
+echo "Welcome";
+});
+Route::get('/show/{id}', function ($id) {
+echo "Nilai Parameter Adalah ".$id;
+});
+Route::get('/show/{id?}', function ($id=1) {
+echo "Nilai Parameter Adalah ".$id;
+});
+Route::get('/edit/{nama}', function ($nama) {
+echo "Nilai Parameter Adalah ".$nama;
+})->where('nama','[A-Za-z]+');
+Route::get('/index', function () {
+echo "<a href='".route('create')."'>Akses Route dengan nama </a>";
+});
+Route::get('/create', function () {
+echo "Route diakses menggunakan nama";
+})->name('create');
+Route::get('/produk', 'produkController@index');
+Route::get('/halaman',function(){
+$title = 'Harry Pooter';
+$konten = 'harry potter and the deathly hallows: part 2';
+return view('konten.halaman',compact('title','konten'));
+});
+?>
